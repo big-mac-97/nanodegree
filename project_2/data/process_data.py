a021d77cb1fea20bd30c678b8e9d467f34ce68fd
McIndoe <euan.mcindoe@angloamerican.com>
2022-01-06 10:37:54 +1000

    uploading process_data.py after copying over completed notebook segments.

--- project_2/data/process_data.py
+++ project_2/data/process_data.py
@@ -1,16 +1,72 @@
+# import libraries
+import pandas as pd
+from sqlalchemy import create_engine
 import sys
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Imports and merges messages and categories data from 2 separate .csv filepaths.
 
+    Parameters:
+    messages_filepath: path of messages csv file
+    categories_filepath: path of categories csv file
+
+    Returns:
+    df: dataframe of merged messages and categories files
+
+    """
+
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on = 'id')
+    print("Imported datasets.")
+
+    return df
 
 def clean_data(df):
-    pass
+    """
+    Cleans the merged messages/categories dataframe.
 
+    Parameters:
+    df: dataframe (generated from load_data())
+
+    Returns:
+    df: Cleaned dataframe with column headers corresponding to categories and values of 1 or 0
+
+    """
+    #creates a dataframe of the column categories and selects the first row to extract column names
+    categories = df['categories'].str.split(pat=";", expand=True)
+    row = categories.iloc[0]
+
+    #generate a Series containing the category names, and rename the df column labels
+    category_colnames = row.apply(lambda x: x[0:(len(x) - 2)])
+    categories.columns = category_colnames
+
+    #extracts last character of each string (the category indicator) and converts to a numeric value
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    #remove now-redundant categories column and append the new cleaned data
+    df.drop(columns="categories", inplace=True)
+    df = df.join(categories)
+    print("Cleaned data.")
+
+    #remove any duplicated messages.
+    df.drop_duplicates(subset="message", inplace=True)
+    print("Removed duplicate messages from dataset.")
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    """Loads cleaned df into a SQLite database."""
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('messages_database', engine, index=False, if_exists='replace')
+    
 
 
 def main():
